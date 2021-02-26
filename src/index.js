94fed2df2fc173ea16f39aa65eaca4e6808c7d94
bpachhai <bpachhai@uno.edu>
2021-02-26 17:10:23 -0600

    Added github pages config for hosting

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 
 ReactDOM.render(
